1314803e3275f172297d2915242db0ef25134f05
mkielak <kielakowsky17@gmail.com>
2025-12-17 21:19:35 +0100

    Added fetching meals

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 const Meals = () => {
+  const [isFetching, setIsFetching] = useState(false);
+  const [meals, setMeals] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setIsFetching(true);
+    const fetchMeals = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/meals')
+        if (!response.ok) {
+          throw new Error('Failed to fetch meals.')
+        }
+        const data = await response.json();
+        setMeals(data);
+      } catch (err) {
+        setError(err.message);
+      }
+      setIsFetching(false);
+    }
+    fetchMeals();
+  }, [])
+
+
   return (
-    <div>Meals</div>
+    <div>
+      {isFetching && <p>Loading meals...</p>}
+      {error && <p>{error}</p>}
+      {!isFetching && !error && (
+        <ul>
+          {meals.map((meal) => (
+            <li key={meal.id}>
+              <h2>{meal.name}</h2>
+              <p>{meal.description}</p>
+              <p>${meal.price}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
   )
 }
 
